31f3804f2b710d25a54654f8800908975945649c
VNOFFICIAL <vardhank319@gmail.com>
2026-09-08 11:10:17 +0530

    Cors allowed to Vercel

--- FirstProject/settings.py
+++ FirstProject/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173/']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173/', 'https://practice-vercel-deploy-iht1kzuh9-pro-7f60.vercel.app/']
